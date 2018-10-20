d2a668b67681beff92bc10332d75fb0eeb7f01c1
TimurSaidov <tim77777@inbox.ru>
2018-10-20 23:21:33 +0300

    Initial Commit

---
+++ Hotel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hotel
+//
+//  Created by Timur Saidov on 20/10/2018.
+//  Copyright © 2018 Timur Saidov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
